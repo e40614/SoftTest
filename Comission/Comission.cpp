f648a90fd89d07b69de66f9c47e36abdd09e82f4
Liumy <e40614@gmail.com>
2014-10-20 14:00:44 -0700

    implement feature code

--- Comission/Comission.cpp
+++ Comission/Comission.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
-#include "Comission.h"
+
 using namespace std;
 
-float Comission(int lock, int stock, int barrel) 
+float Comission(int lock , int stock, int barrel) 
 {
-	return 0;
+	float comission = 0;
+	float lockPrice = 45,stockPrice = 30,barrelPrice = 25;
+	float sales = 0;
+	sales = lock*lockPrice + stock*stockPrice + barrel*barrelPrice;
+	if(sales > 1800) 
+		comission = 0.1*1000 + 0.15*800 + 0.2*(sales-1800);
+	else if(sales > 1000)
+		comission = 0.1*1000 + 0.15*(sales-1000);
+	else
+		comission = 0.1*sales;
+	return comission;
 }
