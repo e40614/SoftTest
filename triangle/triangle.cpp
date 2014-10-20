5fe3f4a8b09a107711920a82c6a1d4859650f90f
Liumy <e40614@gmail.com>
2014-10-20 04:01:59 -0700

    triangle.cpp : check input is in scope

--- triangle/triangle.cpp
+++ triangle/triangle.cpp
@@ -2,6 +2,7 @@
 //input range is 1~200
 triangle_t triangle(int a, int b, int c) 
 {
+	if( a<1 || a>200 || b<1 || b>200 || c<1 || c>200 ) return Error;
 	if( (a < b+c) && (b < a+c) && (c < a+b) ) {
 		if( (a==b) && (b==c) ) return Equilateral;
 		else if( (a!=b) && (a!=c) && (b!=c) ) return Scalene;
